f9e3bb110ad8f88a4d2d8a1c5e69059a06cff627
Akash Manhas <122945144+AkashRajput90@users.noreply.github.com>
2023-10-31 14:28:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import pytest
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,9 +8,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
-
-    tester = app.test_client()
-    response = tester.get('/')
-    assert response.status_code == 200
-    assert response.data == b'Hello, World!'
 
